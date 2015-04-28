c765808a8dd0ca066494c005bb449b8a727902cb
leggetter <phil@leggetter.co.uk>
2015-04-28 22:57:44 +0100

    basics are working

---
+++ test/test.js
@@ -0,0 +1,15 @@
+var expect = require('expect.js');
+
+var Zap = require(__dirname + '/../index.js');
+
+// describe('something', function(){
+//   
+//   it('should dom something', function() {
+//     var easy = "subject|things"
+//     var obj = Zap.dictStringToObject(easy)
+//     expect(obj).to.be.an('object');
+//     expect(obj.subject).to.be.ok();
+//     expect(obj.subject).to.be('things');
+//   });
+//   
+// });
